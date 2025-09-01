ad8d6f7dc2db0ae0fc065580c8a050d7643d2de5
Moniruzzaman <moniruzzaman.barbhuiya@gmail.com>
2025-09-02 04:45:28 +0530

    settimeout was added as well as buttons position was changed and congrats message was added. And some more changes were done

--- src/script.js
+++ src/script.js
@@ -4,6 +4,9 @@ let players = {
 };
 let player1Button = document.querySelector('.show-player1-wins-count');
 let player2Button = document.querySelector('.show-player2-wins-count');
+const resetButton = document.querySelector('.reset-btn');
+const playButton = document.querySelector('.play-again-btn');
+const buttonContainer = document.querySelector('.button-container');
 
 let winsMatch = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
 [2, 4, 6], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
@@ -48,11 +51,13 @@ function showWinner(firstValue) {
     paraElement.innerHTML = `The winner is : <span class="winners-identifier">${firstValue}</span>`;
     let winnersIdentifier = document.querySelector('.winners-identifier');
     if (firstValue === getPlayerName1()) {
+        enteredValue = false; // this line make sure that winner gets the first turn after winning, it makes (!enteredValue) true and the first player gets turn again if wins
         players['player1-wins']++;
         player1Button.innerHTML = `${firstValue} wins : <span class="wins-count">${players['player1-wins']}</span>`;
         winnersIdentifier.classList.add('style-of-x');
 
     } else if (firstValue === getPlayerName2()) {
+        enteredValue = true; // this line make sure that winner gets the first turn after winning, it makes (!enteredValue) true and the second player gets turn again if wins
         players['player2-wins']++;
         player2Button.innerHTML = `${firstValue} wins : <span class="wins-count">${players['player2-wins']}</span>`;
         winnersIdentifier.classList.add('style-of-o');
@@ -60,7 +65,21 @@ function showWinner(firstValue) {
     }
 }
 
-
+let congratsContainer = document.querySelector('.congrats-container');
+let gridContainer = document.querySelector('.grid-container');
+function congrastTheWinner(firstValue) {
+    setTimeout(() => {
+        btnEle.forEach((boxes) => {
+            boxes.classList.add('boxes-grid-visibility');
+        });
+        if (firstValue === getPlayerName1()) {
+            congratsContainer.innerHTML = `<span class="style-of-o">Congratulations!</span>  <span class="style-of-x">${firstValue}</span>`;
+        } else if (firstValue === getPlayerName2()) {
+            congratsContainer.innerHTML = `<span class="style-of-x">Congratulations!</span>  <span class="style-of-o">${firstValue}</span>`;
+        }
+        buttonContainer.classList.add('button-container-position');
+    },300);
+}
 
 
 function checkWinnersMatch() {
@@ -72,8 +91,9 @@ function checkWinnersMatch() {
         if (firstValue != '' && secondValue != '' && thirdValue != '') {
             if (firstValue === secondValue && secondValue === thirdValue) {
                 disabledButton();
-                showWinner(firstValue);
                 winnerFound = true;
+                showWinner(firstValue);
+                congrastTheWinner(firstValue);
                 break;
 
             }
@@ -81,25 +101,27 @@ function checkWinnersMatch() {
         }
     }
     if (!winnerFound) {
-        paraElement.innerHTML = `No winner was found yet`;
+        paraElement.innerHTML = `No winner so far`;
     }
 
 }
 
-document.querySelector('.reset-btn')
-    .addEventListener('click', () => {
-        for (let button of btnEle) {
-            button.innerText = '';
-            enabledButton();
+resetButton.addEventListener('click', () => {
+    for (let button of btnEle) {
+        button.innerText = '';
+        enabledButton();
+        button.classList.remove('boxes-grid-visibility');
 
-        }
-        player1.value = '';
-        player2.value = '';
-        player1Button.innerHTML = `<span class="wins-count">Player1 wins :</span> ${players['player1-wins'] = 0}`;
-        player2Button.innerHTML = `<span class="wins-count">Player2 wins :</span> ${players['player2-wins'] = 0}`;
-        paraElement.innerHTML = `No winner was found yet`;
-        enteredValue = false;
-    });
+    }
+    player1.value = '';
+    player2.value = '';
+    player1Button.innerHTML = `<span class="wins-count">Player1 wins :</span> ${players['player1-wins'] = 0}`;
+    player2Button.innerHTML = `<span class="wins-count">Player2 wins :</span> ${players['player2-wins'] = 0}`;
+    paraElement.innerHTML = `No winner so far`;
+    congratsContainer.innerHTML = '';
+    buttonContainer.classList.add('button-container-position');
+    enteredValue = false; // this line make the game start from player1 after resetting and this line if (!enteredValue) runs 
+});
 
 
 
@@ -108,14 +130,17 @@ function enabledButton() {
         button.disabled = false;
     }
 }
+playButton.addEventListener('click', () => {
+    for (let button of btnEle) {
+        button.innerText = '';
+        enabledButton();
+        button.classList.remove('boxes-grid-visibility');
 
-document.querySelector('.play-again-btn')
-    .addEventListener('click', () => {
-        for (let button of btnEle) {
-            button.innerText = '';
-            enabledButton();
-        }
-    });
+    }
+    congratsContainer.innerHTML = '';
+    buttonContainer.classList.add('button-container-position');
+
+});
 
 
 function disabledButton() {
